691da50457dbf20c079e67b1682f28055d94bd26
sitaram02 <sitaramganesh@gmail.com>
2018-12-20 21:56:17 +0000

    edited

---
+++ simple/src/main/java/imc/formac/ram/App.java
@@ -0,0 +1,13 @@
+package imc.formac.ram;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
